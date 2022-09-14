bd3e335eb6511376c647468b992236dd940b8078
ArbitraryOstrich <mason.thomas.kevin@gmail.com>
2022-09-14 19:35:49 -0400

    Assigment2 helloworld program

---
+++ Assignment2/README.md
@@ -0,0 +1,1 @@
+Assignment 2 was Create a Git repo with a .gitignore suited for python projects and a working python script.
---
+++ Assignment2/hello_world.py
@@ -0,0 +1,6 @@
+thingToSay='Hello World!'
+def main():
+    print(f"This is my '{thingToSay}' program")
+
+if __name__ == "__main__":
+    main()
